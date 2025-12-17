b6c334fae3efd9e1684f6444414c5b98feed0350
fareha1069 <farehailyas6@gmaill.com>
2025-12-18 00:27:01 +0500

    rag implemented

---
+++ IntelliHire.AI/scripts/run_pipeline.py
@@ -0,0 +1,23 @@
+
+
+# numpy
+# scikit-learn
+# openai
+# langchain-anthropic
+# langchain-huggingface
+# transformers
+# huggingface-hub
+# langchain-openai
+# langchain
+# langchain-core
+# langchain-google-genai
+# google-generativeai
+
+# langchain-community 
+# PyMuPDF 
+# tiktoken 
+# faiss-cpu
+# langchain-text-splitters
+
+import langchain
+print(langchain.__version__)
