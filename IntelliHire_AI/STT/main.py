747794f4ff047b12ccd39fa7c6e54782eb2731c8
fareha1069 <farehailyas6@gmail.com>
2026-01-24 13:03:31 +0500

    Remove venv

---
+++ IntelliHire_AI/STT/main.py
@@ -0,0 +1,19 @@
+import aiohttp_cors
+from aiohttp import web
+from STT.route import offer
+
+app = web.Application()
+
+# Setup CORS
+cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+        allow_credentials=True,
+        expose_headers="*",
+        allow_headers="*",
+    )
+})
+cors.add(app.router.add_post("/offer", offer))
+
+if __name__ == "__main__":
+    print("🚀 Python AI service running at http://localhost:8080")
+    web.run_app(app, port=8080)
