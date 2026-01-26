645f7aa0d683dbc66d65e94534a4cf7c5b5240b4
fareha1069 <farehailyas6@gmail.com>
2026-01-26 22:40:56 +0500

    Remove environment

--- IntelliHire.Server/src/index.js
+++ IntelliHire.Server/src/index.js
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
