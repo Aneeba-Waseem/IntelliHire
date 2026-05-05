5a125f67cd0017d3569dbbcdbafa57f54ceb8d31
Aneeba Waseem <aneeba.waseem145@gmail.com>
2026-05-05 11:58:15 +0500

    Remove server start code from index.js
    Remove server listening code from index.js

--- IntelliHire.Server/src/index.js
+++ IntelliHire.Server/src/index.js
@@ -20,5 +20,3 @@ console.log("DB_HOST:", process.env.DB_HOST);
 console.log("DB_NAME:", process.env.DB_NAME);
 console.log("DB_PORT:", process.env.DB_PORT);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
