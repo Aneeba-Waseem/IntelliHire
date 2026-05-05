cff828d6962674ddafe71da073d9da168012952a
fareha1069 <farehailyas6@gmail.com>
2026-05-05 21:14:59 +0500

    changing redis function name

--- IntelliHire.Server/src/controllers/finalizeHiringController.js
+++ IntelliHire.Server/src/controllers/finalizeHiringController.js
@@ -59,7 +59,8 @@ export const finalizeHiring = async (req, res) => {
         }
 
         const batchId = await redis.get(`job:${userId}:step2:batchId`);
-        const resumes = await redis.hGetAll(batchId);
+        const resumes = await redis.hgetall(key)
+        //  await redis.hGetAll(batchId);
 
         const shortlistedIds = interviews.map(i => i.resume_id);
 
